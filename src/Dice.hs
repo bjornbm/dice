7388409f03b9d3104a60a577e17c4eb0ce409698
Björn Buckwalter <bjorn@buckwalter.se>
2019-04-02 16:29:17 +0200

    Allow dice combos and modifiers.

--- src/Dice.hs
+++ src/Dice.hs
@@ -1,6 +1,7 @@
 module Dice where
 
 import Control.Monad (replicateM, join)
+import Data.List (intercalate)
 import Data.Ratio
 import System.Random
 
@@ -17,35 +18,52 @@ instance Show Odds where
       n = numerator   r
       d = denominator r
 
+instance Semigroup Dice where
+  d1 <> d2 = Different [d1, d2]
 
-data Dice = Int`D`Int
-instance Show Dice where show (D n m) = show n ++ "D" ++ show m
-d = D
-d2   = (`D`  2)
-d3   = (`D`  3)
-d4   = (`D`  4)
-d6   = (`D`  6)
-d8   = (`D`  8)
-d10  = (`D` 10)
-d12  = (`D` 12)
-d20  = (`D` 20)
-d30  = (`D` 30)
-d100 = (`D`100)
+data Dice = One { sides :: Int }
+          | Many { count :: Int, sides :: Int }
+          | Different { dice :: [Dice] }
+          | Modifier { modifier :: Int }
+instance Show Dice where
+  show (One n) = "D" ++ show n
+  show (Many m n) = show m ++ "D" ++ show n
+  show (Different ds) = intercalate "+" $ map show ds  -- TODO not good for modifiers
+  show (Modifier i) = show i
 
-outcomes :: Dice -> [[Int]]
-outcomes (n`D`m) = replicateM n [1..m]
+d `plus`  i = d <> Modifier i
+d `minus` i = d <> Modifier (negate i)
 
-outcomes' :: [Dice] -> [[Int]]
-outcomes' = map join . mapM (outcomes)
+d = Many
+d2   = One   2
+d3   = One   3
+d4   = One   4
+d6   = One   6
+d8   = One   8
+d10  = One  10
+d12  = One  12
+d20  = One  20
+d30  = One  30
+d100 = One 100
 
-outcomesN :: Dice -> Int
-outcomesN (n`D`m) = m ^ n
+-- | The possible sum of the .
+outcomes1 :: Dice -> [Int]
+outcomes1 (One n) = [1..n]
+outcomes1 (Many m n) = map sum . replicateM m $ outcomes1 (One n)
+outcomes1 (Modifier i) = [i]
+outcomes1 (Different ds) = map sum $ mapM outcomes1 ds
 
-rate :: ([Int] -> Bool) -> Dice -> Rate
-rate p d = length (filter p $ outcomes d) % outcomesN d
-percent :: ([Int] -> Bool) -> Dice -> Percent
+outcomes :: [Dice] -> [[Int]]
+outcomes = mapM outcomes1
+
+outcomesN :: [Dice] -> Int
+outcomesN = length . outcomes
+
+rate :: ([Int] -> Bool) -> [Dice] -> Rate
+rate p ds = length (filter p $ outcomes ds) % outcomesN ds
+percent :: ([Int] -> Bool) -> [Dice] -> Percent
 percent p = Percent . rate p
-odds :: ([Int] -> Bool) -> Dice -> Odds
+odds :: ([Int] -> Bool) -> [Dice] -> Odds
 odds    p = Odds    . rate p
 
 
@@ -57,8 +75,8 @@ sample xs gen = (xs !! i, g) where (i, g) = randomR (0, length xs - 1) gen
 sampleIO :: [a] -> IO a
 sampleIO xs = (xs !!) <$> randomRIO (0, length xs - 1)
 
-roll :: Dice -> IO [Int]
-roll = sampleIO . outcomes
+roll :: Dice -> IO Int
+roll = sampleIO . outcomes1
 
-roll' :: Int -> (Int -> Int -> Dice) -> Int -> IO [Int]
+roll' :: Int -> (Int -> Int -> Dice) -> Int -> IO Int
 roll' n d m = roll (n`d`m)
